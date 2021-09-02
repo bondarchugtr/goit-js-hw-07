23ca2d0894f1daa2cabc5a7ab5136b5081c12fc0
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-02 09:30:22 +0300

    task5+

--- js/task-4.js
+++ js/task-4.js
@@ -1,39 +1,20 @@
 const refs = {
     decrementBtn: document.querySelector('[data-action="decrement"]'),
     incrementBtn: document.querySelector('[data-action="increment"]'),
-    values: document.querySelector('#value')
+    valueSpanBtn: document.querySelector('#value')
 }
+
 refs.decrementBtn.addEventListener('click', onDecrementBtn);
 refs.incrementBtn.addEventListener('click', onIncrementBtn)
 
-function addValue() {
-
-    let counterValue = 0;
-    function decrementBtn() {
-        return (counterValue -= 1)
-    }
-
-    function incrementBtn() {
-        return (counterValue += 1);
-    }
-
-    return { decrementBtn, incrementBtn }
-};
+let counterValue = 0;
 
 function onDecrementBtn(event) {
+    refs.valueSpanBtn.textContent = counterValue -= 1;
     console.log(event)
 }
 function onIncrementBtn(event) {
+    refs.valueSpanBtn.textContent = counterValue += 1;
     console.log(event)
 }
-
-
-// const btnDecr = document.querySelector('[data-action="decrement"]');
-// const btnIncr = document.querySelector('[data-action="increment"]');
-// const spanVal = document.querySelector("#value");
-// let counterValue = 0;
-// const increment = () => (spanVal.textContent = counterValue += 1);
-// const decrement = () => (spanVal.textContent = counterValue -= 1);
-// btnDecr.addEventListener("click", decrement);
-// btnIncr.addEventListener("click", increment);
 

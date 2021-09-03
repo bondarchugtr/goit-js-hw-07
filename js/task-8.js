5fe5807ad2d544bf52a6bacca150296270079d86
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-03 20:28:13 +0300

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -1,27 +1,41 @@
 const refs = {
-    container: document.querySelector('#controls'),
+    container: document.querySelectorAll('button[data-action]'),
     boxesContainer: document.querySelector('#boxes'),
     input: document.querySelector('input'),
     renderBt: document.querySelector('[data-action="render"]'),
     destroyBt: document.querySelector('[data-action="destroy"]'),
 }
 
-console.log(refs.destroyBt)
-
-const onInputOption = (event) => {
-    console.log(event)
-}
-
 const onRenderBtAddElents = (event) => {
-    refs.container += refs.input.value;
-    console.log(event)
-}
+    const creatBoxes = [];
+    let widthBt = 30;
+    let hightBT = 30;
+    const step = 10
 
+    for (let el = 0; el < event; el += 1) {
+        creatBoxes.push(document.createElement('div'))
+        creatBoxes[el].style.width = widthBt += 'px'
+        creatBoxes[el].style.height = hightBT += 'px'
+        creatBoxes[el].style.backgroundColor = 'rgb(' +
+            Math.floor(Math.random() * 255) + 1 + ',' + Math.floor(Math.random() * 255) + 1
+            + ',' + (Math.floor(Math.random() * 255));
+        widthBt += step;
+        hightBT += step;
+    }
+    refs.boxesContainer.append(...creatBoxes)
+}
 const onDestroyBtRemoveElents = (event) => {
-    refs.boxesContainer.value = ''
-    console.log(event)
-}
+    refs.boxesContainer.innerHTML = ''
+    refs.input.value = ''
 
-refs.input.addEventListener('click', onInputOption);
-refs.renderBt.addEventListener('click', onRenderBtAddElents);
-refs.destroyBt.addEventListener('click', onDestroyBtRemoveElents);
+}
+refs.container.forEach((ell) => {
+    if (ell === refs.renderBt) {
+        ell.addEventListener('click', () => {
+            onRenderBtAddElents(refs.input.value)
+
+        })
+    } else {
+        ell.addEventListener('click', onDestroyBtRemoveElents)
+    }
+})
